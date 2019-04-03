f4166d4623f6c280297d925e044509ad145357d8
francoMG <francomolocheg99@gmail.com>
2019-04-03 17:44:33 -0500

    v0.03

--- app/src/main/java/com/temp/user/matecomputacional_huffman/MainActivity.java
+++ app/src/main/java/com/temp/user/matecomputacional_huffman/MainActivity.java
@@ -2,6 +2,12 @@ package com.temp.user.matecomputacional_huffman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.mainButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText input = (EditText)findViewById(R.id.mainInput);
+                TextView toModify = (TextView)findViewById(R.id.mainOutput);
+                String text = input.getText().toString();
+
+                String freqs = "";
+
+                HashMap<Character, String> code = Compress.compress(text);
+                for (HashMap.Entry<Character, String> entry : code.entrySet()) {
+                    freqs += "\n" + (entry.getKey() + " : " + entry.getValue());
+                }
+
+                System.out.println(freqs);
+
+                toModify.setText(freqs);
+            }
+        });
     }
 }
